5331f744d9b622bab53e6a9e9a6f420de2969cd5
Kfir <74012686+kfiree@users.noreply.github.com>
2020-11-29 17:02:16 +0200

    add nodeData, DS_DWGraph and Algo_DWGraph classes
    Signed-off-by: Kfir <74012686+kfiree@users.noreply.github.com>

---
+++ src/api/nodeData.java
@@ -0,0 +1,48 @@
+package api;
+
+public class nodeData implements node_data {
+    @Override
+    public int getKey() {
+        return 0;
+    }
+
+    @Override
+    public geo_location getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(geo_location p) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double w) {
+
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    @Override
+    public int getTag() {
+        return 0;
+    }
+
+    @Override
+    public void setTag(int t) {
+
+    }
+}
