ce1813120a1ef4663788632c9e82db6bad80179f
Tehila <74303333+tehilabk@users.noreply.github.com>
2020-12-14 00:53:40 -0800

    load graph from string

--- src/gameClient/Ex2.java
+++ src/gameClient/Ex2.java
@@ -1,4 +1,20 @@
 package gameClient;
 
+import api.DS_DWGraph;
+import api.node_data;
+
+import java.util.List;
+import java.util.Set;
+
 public class Ex2 {
 }
+
+
+
+
+
+
+//public List<Set<node_data>> getScc (DS_DWGraph graph){
+//
+//
+//}
